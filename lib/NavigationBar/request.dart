73e39c213907d2988a4783b3ceb809ff2b83f210
Sanali Losathi <kklosathi19@gmail.com>
2023-09-16 21:45:06 +0530

    added request dart file

---
+++ lib/NavigationBar/request.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RequestPage extends StatelessWidget {
+  const RequestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
