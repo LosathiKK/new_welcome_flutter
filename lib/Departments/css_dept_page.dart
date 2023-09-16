07a37f9bbfb3ec8e34fc777c6613668c35241a09
Sanali Losathi <kklosathi19@gmail.com>
2023-09-16 21:51:11 +0530

    added csse_dept_pg dart file

---
+++ lib/Departments/css_dept_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CsseDepPage extends StatelessWidget {
+  const CsseDepPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
