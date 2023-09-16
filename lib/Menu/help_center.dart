8d32cf5c4c3508b601ed92dbec788a0d0846f904
Sanali Losathi <kklosathi19@gmail.com>
2023-09-16 21:47:52 +0530

    added help_center dart file

---
+++ lib/Menu/help_center.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelpCenterPage extends StatelessWidget {
+  const HelpCenterPage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
