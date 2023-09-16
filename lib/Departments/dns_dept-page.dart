4e4b4042c4b5f06f26e679477058f85be364e039
Sanali Losathi <kklosathi19@gmail.com>
2023-09-16 21:51:54 +0530

    added dns_dept_pg dart file

---
+++ lib/Departments/dns_dept-page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DnsDeptPage extends StatelessWidget {
+  const DnsDeptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
