09bedaf3e9629c8a74d40d1075e69400ab049564
Sanali Losathi <kklosathi19@gmail.com>
2023-09-16 21:46:07 +0530

    added schedule dart file

---
+++ lib/NavigationBar/schedule.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SchedulePage extends StatelessWidget {
+  const SchedulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+    );
+  }
+}
